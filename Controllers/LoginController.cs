3cac6de2b07f6c8be397298ebf8829169a2e3612
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-02 09:42:44 +0300

    Added LoginController

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shared_Clipboard_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : Controller
+    {
+        
+    }
+}
