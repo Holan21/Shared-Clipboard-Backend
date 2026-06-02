16a01b08d48464ec6d2608d881ff56211d5804bf
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-02 09:49:04 +0300

    Remove LoginController, add AuthController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shared_Clipboard_Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return null;
+        }
+    }
+}
