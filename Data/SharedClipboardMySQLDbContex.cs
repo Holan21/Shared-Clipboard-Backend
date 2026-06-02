d0c78417075734b6798c6cacecaa5fbf6f0aee13
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-02 16:10:08 +0300

    Added DbContext

---
+++ Data/SharedClipboardMySQLDbContex.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared_Clipboard_Backend.Models;
+
+namespace Shared_Clipboard_Backend.Data
+{
+    public class SharedClipboardMySQLDbContex : DbContext
+    {
+        DbSet<User> Users { get; set; }
+        DbSet<Devices> Devices { get; set; }
+        DbSet<SharedClipboard> SharedClipboard { get; set; }
+
+    }
+}
--- Extensions/ConfigApplication/DatabaseConfigurationExtensions.cs
+++ Extensions/ConfigApplication/DatabaseConfigurationExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace Shared_Clipboard_Backend.Extensions.ConfigApplication
+﻿using Shared_Clipboard_Backend.Data;
+
+namespace Shared_Clipboard_Backend.Extensions.ConfigApplication
 {
     public static class DatabaseConfigurationExtensions
     {
         public static WebApplicationBuilder AddDatabase(this WebApplicationBuilder builder)
         {
-            //builder.Services.AddDbContext();
+            builder.Services.AddDbContext<SharedClipboardMySQLDbContex>();
             return builder;
         }
 
