520fc76e5b63039320b887b98eda748177d6e465
Holan21 <anton.kramarenko.holan@gmail.com>
2026-06-04 09:33:18 +0300

    Added JwtProvider

---
+++ Models/Options/JwtOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared_Clipboard_Backend.Models.Options
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public int ExpiresHours { get; set; }
+    }
+}
--- Services/JwtProvider/JwtProvider.cs
+++ Services/JwtProvider/JwtProvider.cs
@@ -1,14 +1,32 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Shared_Clipboard_Backend.Models.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Shared_Clipboard_Backend.Services.JwtProvider
 {
     public class JwtProvider
     {
+        private readonly JwtOptions _options;
+
+        public JwtProvider(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
         public async Task<string> Generate()
         {
-            JwtSecurityToken jwt = new JwtSecurityToken() {  };
+            var signingCreadentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
 
-            return string.Empty;
+            var token = new JwtSecurityToken(
+                signingCredentials:signingCreadentials,
+                expires: DateTime.UtcNow.AddHours(_options.ExpiresHours)
+                );
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenValue;
         }
     }
 }
